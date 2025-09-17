4d6c4a842767b72ec7bbc07a996a68461c4da211
RicardoPerezGurrola <richygunz88@hotmail.com>
2025-09-17 14:53:01 -0600

    Imagenes completas

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -27,7 +27,7 @@ const Projects: React.FC = () => {
       id: 2,
       title: 'Control de botellas',
       description: 'Se realizo una aplicacion en la cual se pudiera llevar un control de el contenido y control de lotes de una variedad de botellas alcoholicas,sabiendo el numero de OZ, ML y % restantes de cada botela con un ID interno por cada botella para identifiarlas, asi como tambien el costo por OZ Y ML de cada una. Para el front se utilizo Java Script, HTML Y TAILWINDCSS en conjunto con la libteria Daisy UI para el diseño de la pagina Y para la parte de el bakend se utilizo Node.js con el framework de Express.js',
-      image: `${import.meta.env.BASE_URL}images/project2.svg`,
+      image: `${import.meta.env.BASE_URL}images/Control_De_Botellas.svg`,
       technologies: ['Vite', 'Node.js', 'Express', 'JavaScript', 'HTML', 'CSS', 'SQL Server', 'Tailwind CSS'],
       githubUrl: 'https://github.com/username/project2'
     }
